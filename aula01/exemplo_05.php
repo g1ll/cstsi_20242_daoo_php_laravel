5de91913208136241725b28b8f8b4942e8235abf
g1ll <gillvelleda@gmail.com>
2024-10-11 09:01:03 -0500

    Exemplos de herança e modificadores de acesso.

--- aula01/exemplo_05.php
+++ aula01/exemplo_05.php
@@ -18,13 +18,21 @@ class Pessoa {
 		echo "\n$this->nome será removido da memória\n";
 	}
 
-	function calcImc(){
+	public function calcImc(){
 		if(!$this->peso && !$this->altura){
 			return "Erro: configura o peso e altura primeiro!!!";
 		}
-		
+
 		$this->imc = $this->peso/$this->altura**2;
 		return number_format($this->imc,2);
+	}
+
+	public function getImc(){
+		return $this->imc;
+	}
+
+	public function setImc($valor){
+		$this->imc = $valor;
 	}
 
 }
@@ -34,5 +42,12 @@ $pessoaDois = new Pessoa("Vera",60,1.55,89);
 
 echo "\nImc do $pessoaUm->nome eh ".$pessoaUm->calcImc()."\n";
 echo "\nImc do $pessoaDois->nome eh ".$pessoaDois->calcImc()."\n";
+
+$pessoaTres = new Pessoa("Fulano",40);
+
+$pessoaTres->setImc(25.5);
+echo "\nImc do $pessoaTres->nome eh ".$pessoaTres->getImc()."\n";
+
+// $pessoaDois->imc;//Erro
 
 echo "Script será encerrado!!!";
