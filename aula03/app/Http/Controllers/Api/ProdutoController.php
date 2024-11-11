d9eeae0257c96012e34d23209362425618bff10d
g1ll <gillvelleda@gmail.com>
2024-11-11 08:54:08 -0500

    feat: validando requisição na classe ProdutoStoreRequest. php artisan make:request ProdutoStoreRequest

--- aula03/app/Http/Controllers/Api/ProdutoController.php
+++ aula03/app/Http/Controllers/Api/ProdutoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ProdutoStoreRequest;
 use App\Http\Resources\ProdutoCollection;
 use App\Http\Resources\ProdutoResource;
 use App\Models\Produto;
@@ -24,26 +25,15 @@ class ProdutoController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(ProdutoStoreRequest $request)
     {
 
         try {
-            $request->validate([
-                "nome" => "required | max: 10",
-                "importado" => "nullable | boolean",
-                "qtd_estoque" => "required | numeric | min:2",
-                "descricao" => "required | max:500",
-                "preco" => "required | numeric | min:1.99"
-            ]);
-            $produto = $request->all();
-            $produto['importado'] = $request->has('importado');
-            $produtoCriado = Produto::create($produto);
+            $produtoCriado = Produto::create($request->validated());
             return (new ProdutoResource($produtoCriado))
                     ->additional(["message" => 'Produto Criado!!!'])
                     ->response()
                     ->setStatusCode(201,"Produto Criado!");
-        }catch(ValidationException $error){
-            throw $error;
         }catch (Exception $error) {
             $httpStatus = 500;
             $message = 'Erro ao criar novo produto!!!';
---
+++ aula03/app/Http/Requests/ProdutoStoreRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProdutoStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "nome" => "required | max: 10",
+            "importado" => "nullable | boolean",
+            "qtd_estoque" => "required | numeric | min:2",
+            "descricao" => "required | max:500",
+            "preco" => "required | numeric | min:1.99"
+        ];
+    }
+
+    protected function prepareForValidation(): void
+    {
+        $this->merge([
+            'importado'=>$this->has('importado')
+        ]);
+    }
+}
