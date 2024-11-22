8cde64b986bf7cdddbbaf096e49607e1758abfc1
g1ll <gillvelleda@gmail.com>
2024-11-22 09:32:14 -0500

    feat: proteção de rotas da api.

--- aula03/routes/api.php
+++ aula03/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\ProdutoController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,4 +11,15 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 
-Route::apiResource('/produtos',ProdutoController::class);
+Route::apiResource('/produtos',ProdutoController::class)->middleware('auth:sanctum');
+Route::apiResource('/produtos',ProdutoController::class)->only(['index','show']);
+
+Route::apiResource('/users',UserController::class);
+Route::apiResource('/users',UserController::class)
+    ->middleware('auth:sanctum') //post, put, delete
+    ->except(['index','show']); // não cria as rotas de index e show
+
+Route::controller(LoginController::class)->group(function(){
+    Route::post('/login','login');
+    Route::post('/logout','logout')->middleware('auth:sanctum');
+});
