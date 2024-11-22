06c25e26fdcd7980ca1d4f13fd3a8bae33c43ae9
g1ll <gillvelleda@gmail.com>
2024-11-22 09:36:26 -0500

    refactor: criando rotas com agrupamento.

--- aula03/routes/api.php
+++ aula03/routes/api.php
@@ -11,15 +11,30 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 
-Route::apiResource('/produtos',ProdutoController::class)->middleware('auth:sanctum');
-Route::apiResource('/produtos',ProdutoController::class)->only(['index','show']);
 
-Route::apiResource('/users',UserController::class);
-Route::apiResource('/users',UserController::class)
-    ->middleware('auth:sanctum') //post, put, delete
-    ->except(['index','show']); // não cria as rotas de index e show
+// // Only é após registro de todas as rotas COM  middleware
+// Route::apiResource('/produtos',ProdutoController::class)->middleware('auth:sanctum');
+// Route::apiResource('/produtos',ProdutoController::class)->only(['index','show']);
 
-Route::controller(LoginController::class)->group(function(){
-    Route::post('/login','login');
-    Route::post('/logout','logout')->middleware('auth:sanctum');
+
+// //Except é após o registro de todas as rotas SEM middleware
+// Route::apiResource('/users',UserController::class);
+// Route::apiResource('/users',UserController::class)
+//     ->middleware('auth:sanctum') //post, put, delete com middleware
+//     ->except(['index','show']); // não cria as rotas de index e show
+
+//Agrupando por middleware sanctum todas as rotas de produtos e usuários
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('/produtos', ProdutoController::class);
+    Route::apiResource('/users', UserController::class);
+});
+
+//Liberando apenas as rotas index e show
+Route::apiResource('/produtos', ProdutoController::class)->only(['index', 'show']);
+Route::apiResource('/users', UserController::class)->only(['index', 'show']);
+
+
+Route::controller(LoginController::class)->group(function () {
+    Route::post('/login', 'login');
+    Route::post('/logout', 'logout')->middleware('auth:sanctum');
 });
