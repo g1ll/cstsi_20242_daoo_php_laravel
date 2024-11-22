04b69e058305738bda88444def1bf1ce64072841
gill from r5950x <gillvelleda@gmail.com>
2024-11-22 16:32:47 -0300

    fix: corrigindo o método compact.

--- aula03/app/Http/Controllers/Api/LoginController.php
+++ aula03/app/Http/Controllers/Api/LoginController.php
@@ -4,23 +4,18 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Api\Controller;
 use App\Http\Requests\LoginRequest;
-use App\Models\User;
 use Exception;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
     public function login(LoginRequest $request){
         try{
             $user = $request->user;
-            if(!Hash::check($request->password,$user->password)){
-                throw new Exception('Credenciais inválidas!!!');
-            }
             $token = $user->createToken('token')->plainTextToken;
-            return response()->json(['token' => $token]);
-            // return response()->json(compact($token));
-            // return compact($token);
+            // return response()->json(['token' => $token]);
+            // return response()->json(compact('token'));
+            return compact('token');
         }catch(Exception $error){
             $this->errorHandler('Erro ao realizar login!!!',$error);
         }
