83f69a6a48fb830226a26e936d48c16b239a28b8
g1ll <gillvelleda@gmail.com>
2024-11-22 09:23:00 -0500

    feat: criacao do controller de usuario. php artisan make:controller -m User Api/UserController --resource --api

---
+++ aula03/app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Api\Controller;
+use App\Http\Requests\UserStoreRequest;
+use App\Http\Resources\UserResource;
+use App\Http\Resources\UserResourceCollection;
+use App\Models\User;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return new UserResourceCollection(User::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(UserStoreRequest $request)
+    {
+
+        try {
+            $userParams = $request->validated();
+            $userParams['password'] = Hash::make($userParams['password']);
+            return (new UserResource(User::create($request->validated())))
+                    ->additional(['message' => 'Usuário criado com sucesso!!!']);
+        }catch (Exception $error){
+            $this->errorHandler('Erro ao criar novo usuário!!!',$error);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, User $user)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        //
+    }
+}
