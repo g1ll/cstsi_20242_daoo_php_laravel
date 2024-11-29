a5274c9fe07d99887d11be1039af179b74edbdd0
g1ll <gillvelleda@gmail.com>
2024-11-29 09:54:51 -0500

    feat: atualiza a model de Protudo, a migration e cria a sua factory. Atualiza a model de Produto e sua migration para configurar a relação com a model de Fornecedor. Cria também a factory de produtos para popular no banco de dados.

--- aula03/app/Models/Produto.php
+++ aula03/app/Models/Produto.php
@@ -2,15 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Produto extends Model
 {
+
+    use HasFactory;
+
     protected $fillable = [
         "nome",
         "descricao",
         "qtd_estoque",
         "preco",
-        "importado"
+        "fornecedor_id"
     ];
+
+    public function fornecedor(){
+        return $this->belongsTo(Fornecedor::class);
+    }
 }
